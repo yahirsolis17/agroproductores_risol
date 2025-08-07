4cc2b6f6363c4ac111342ca99d2ac541153263e2
yahirsolis17 <22307013@utcgg.edu.mx>
2025-08-06 21:16:15 -0600

    Import timezone in inversiones views

--- backend/gestion_huerta/views/inversiones_views.py
+++ backend/gestion_huerta/views/inversiones_views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets, status, filters, serializers
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from django.utils import timezone
 
 from gestion_huerta.models import InversionesHuerta
 from gestion_huerta.serializers import InversionesHuertaSerializer
