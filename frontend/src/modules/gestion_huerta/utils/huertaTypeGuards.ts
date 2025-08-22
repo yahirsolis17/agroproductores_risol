d257cb62e1dfac8ead53bd1b084f1efd9e30983d
yahirsolis17 <22307013@utcgg.edu.mx>
2025-08-21 20:36:39 -0600

    Use explicit tipo to handle rentadas

--- frontend/src/modules/gestion_huerta/utils/huertaTypeGuards.ts
+++
@@ -1,8 +0,0 @@
-// src/modules/gestion_huerta/utils/huertaTypeGuards.ts
-import { HuertaRentada } from '../types/huertaRentadaTypes';
-
-/** Devuelve true si el registro es una huerta rentada */
-export function isRentada(h: any): h is HuertaRentada {
-  return 'monto_renta_palabras' in h;
-}
-
