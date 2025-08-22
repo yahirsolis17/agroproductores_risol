80b86f310177c7016de9b18e7467d5c46a95e7a3
Yahir <yahirsolis546@gmail.com>
2025-08-22 01:12:11 -0600

    gestion_huerta
    estructura de reportes de produccion

---
+++ backend/gestion_huerta/utils/reporting.py
@@ -0,0 +1,13 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Punto único para construir el contrato de reportes del front:
+{
+  "kpis": [{ id, label, value, hint? }],
+  "series": [{ id, label, type, data }],
+  "tabla": { columns: [...], rows: [...] }
+}
+Implementaremos: 
+ - aggregates_for_cosecha(), aggregates_for_temporada(), aggregates_for_huerta()
+ - series_for_cosecha(), series_for_temporada(), series_for_huerta()
+"""
+# TODO: implementar funciones arriba mencionadas
---
+++ backend/gestion_huerta/views/informes_views.py
@@ -0,0 +1,12 @@
+﻿# -*- coding: utf-8 -*-
+"""
+ViewSet de informes con 3 acciones GET:
+ - /informes/cosechas/{id}/
+ - /informes/temporadas/{id}/
+ - /informes/huertas/{id}/perfil/
+Devuelve siempre el contrato {kpis, series, tabla}.
+"""
+# TODO: class ReportesViewSet(viewsets.GenericViewSet): ...
+# TODO: @action(detail=True, methods=["get"], url_path="cosechas")
+# TODO: @action(detail=True, methods=["get"], url_path="temporadas")
+# TODO: @action(detail=True, methods=["get"], url_path="huertas/perfil")
